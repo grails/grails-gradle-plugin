c4b2fb0cf95bd0033ba50c15a6c7278e85e1e6ad
Puneet Behl <behlp@objectcomputing.com>
2023-07-24 14:12:55 +0530

    Update default Micronaut version to 3.9.3

--- src/main/groovy/org/grails/gradle/plugin/core/GrailsGradlePlugin.groovy
+++ src/main/groovy/org/grails/gradle/plugin/core/GrailsGradlePlugin.groovy
@@ -195,7 +195,7 @@ class GrailsGradlePlugin extends GroovyPlugin {
     }
 
     protected String getDefaultMicronautVersion() {
-        '3.9.2'
+        '3.9.3'
     }
 
     void addDefaultProfile(Project project, Configuration profileConfig) {
